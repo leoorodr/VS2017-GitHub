60ca7ebb9eb86b6dbce18349fae2951c2c490719
leoorodr <leleu45_melo@hotmail.com>
2020-06-11 17:11:17 -0300

    Primeiro commit

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso do GitHub");
+            Console.ReadKey();
+        }
+    }
+}
